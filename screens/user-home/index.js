53ae949d2a12a9d7e299a0394d2082d6e3c841f3
BryanMM <bryanmasis87@gmail.com>
2019-03-25 19:59:15 -0600

    addeded the image icons on the home view

--- screens/user-home/index.js
+++ screens/user-home/index.js
@@ -6,10 +6,35 @@ import {
   Image,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View,
 } from 'react-native';
+import ParkingAssistance from '../../assets/parkingAssistance.png';
+import GarageObstruction from '../../assets/garageObstruction.png';
+import schedParking from '../../assets/schedParking.png';
 
-
+class ListItem extends React.PureComponent {
+  _onPress = () => {
+    this.props.onPressItem(this.props.index)
+  }
+  render () {
+    const { item } = this.props
+    return (
+      <TouchableHighlight underlayColor = '#dddddd'
+        onPress = {this._onPress}>
+        <View style = {styles.rowContainer}>
+        <Image style = {styles.thumbnail} source = { item.image } />
+          <View style = {styles.textContainer}>
+            <Text styles = {styles.title} numberOflLines = {1}> {item.title}</Text>
+          </View>
+          <View style = {styles.separator}>
+          </View>
+        </View>
+      </TouchableHighlight>
+      
+    )
+  }
+}
 export default class UserLogin extends React.Component {
     static navigationOptions = ({ navigation }) => ({
         title: `${navigation.state.params.title}`
@@ -18,34 +43,40 @@ export default class UserLogin extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          userName: '',
           gridItems: [
-              { title: 'Check for obstacles at the entrance',
-                image: 
+              { 
+                title: 'Check for obstacles at the entrance',
+                image: GarageObstruction
               },
-              { key: 'Parking assistance' },
-              { key: 'Program an automatic garage opening'},
+              { 
+                title: 'Parking assistance',
+                image: ParkingAssistance
+              },
+              { 
+                title: 'Program an automatic garage opening',
+                image: schedParking
+              },
           ]
         };
       }
+    
+    _keyExtrator = (item, index) => index.toString()
 
-    _renderItem = ({ item}) => {
-        return ( 
-            <View style={styles.gridBackground}>
-               <Text style={styles.gridSquare}> 
-               {item.key} 
-               </Text>
-            </View>
-          
-        )
-      }
+    _renderItem = ({ item, index }) => {
+      return ( 
+        <ListItem
+          item = {item}
+          index = {index}
+        />
+      )
+    }
 
     render () {       
         return (
             <View style = { styles.gridBackground }>
                 <FlatList 
-                    style = { styles.gridSquare }
                     data = { this.state.gridItems } 
+                    keyExtractor={this._keyExtrator}
                     renderItem = { this._renderItem }
                     numColumns = {2}
                 /> 
@@ -55,14 +86,29 @@ export default class UserLogin extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    gridBackground: {
-        justifyContent: 'center',
-        flex: 1,
-        paddingTop: 20
-    },
-    gridSquare: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: 100,
-    },
+  thumbnail : {
+    width: 80,
+    height: 80,
+    marginRight :10
+  },
+  textContainer : {
+    flex: 1,
+  },
+  separator : {
+    height: 1,
+    backgroundColor: "#dddddd"
+  },
+  price : {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: "#488BEC"
+  },
+  title : {
+    fontSize: 20,
+    color: "#656565"
+  },
+  rowContainer : {
+    flexDirection: 'row',
+    padding: 10
+  }
   });
