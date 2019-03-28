5e95dbbea531e367c1c1c6da01047dd65d665466
BryanMM <bryanmasis87@gmail.com>
2019-03-28 14:43:42 -0600

    deleted unnecesary imports

--- screens/parking-schedule/index.js
+++ screens/parking-schedule/index.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { Constants } from 'expo';
 
 
 export default class App extends React.Component {
@@ -19,7 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
     padding: 8,
   },
